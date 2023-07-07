876900a9c39b996af6ebb701ef3913357152a4ab
Aayush <aayushj2001@gmail.com>
2023-07-07 16:14:38 +0545

    feat: new changes into the public

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ process.on("uncaughtException", (err) => {
 });
 
 if (process.env.NODE_ENV !== "PRODUCTION")
-  require("dotenv").config({ path: "/config/.env" });
+  require("dotenv").config({ path: "MedhaviHackathon/backend/config/config.env" });
 
 connectDatabase();
 
